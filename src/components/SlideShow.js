d6aeffbbeafccbdb78fbc7dacd4f3fe234375f08
Michelle Oh <ohmotrl@gmail.com>
2024-12-17 23:45:22 -0500

    added state for slide and ternary operator

--- src/components/SlideShow.js
+++ src/components/SlideShow.js
@@ -1,17 +1,24 @@
+import {useState} from "react";
 import { BsCaretLeftSquareFill, BsCaretRightSquareFill } from "react-icons/bs";
 import "./slideShow.css"; 
 
 const SlideShow = ({slides}) => {
+    const [slide, setSlide] = useState(0);
+    
     return ( 
         <div className="slide-show">
             <BsCaretLeftSquareFill className="arrow arrow-left" />
             {slides.map((item, idx) => {
-            return <img src={item.url} alt={item.alt} key={idx} className="slide" />
+                return <img src={item.url} 
+                    alt={item.alt} 
+                    key={idx} 
+                    //Conditional className of slide, if the slide variable is the same as the index of this image, pass it a className of slide. Otherwise, pass it a className of slide and slide-hidden.//  
+                    className={slide === idx ? "slide" : "slide slide-hidden"} />
         })}
             <BsCaretRightSquareFill className="arrow arrow-right" />
             <span className="indicators" >
                 {slides.map((_, idx) => {
-                    return <button key={idx} onClick={null} className="indicator" ></button>
+                    return <button key={idx} onClick={null} className={slide === idx ? "indicator" : "indicator indicator-inactive"} ></button>
                 })}
             </span>
         </div>
