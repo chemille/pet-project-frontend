9e2dcdd1f6bc628a544412182705cc2783a775ce
Michelle Oh <ohmotrl@gmail.com>
2024-12-17 23:59:39 -0500

    next slide and previous slide displaying when arrows are clicked

--- src/components/SlideShow.js
+++ src/components/SlideShow.js
@@ -4,10 +4,18 @@ import "./slideShow.css";
 
 const SlideShow = ({slides}) => {
     const [slide, setSlide] = useState(0);
-    
+
+    const nextSlide = () => {
+        setSlide(slide === slides.length - 1 ? 0 : slide + 1);
+    }
+
+    const previousSlide = () => {
+        setSlide(slide === 0 ? slides.length - 1 : slide - 1);
+    }
+
     return ( 
         <div className="slide-show">
-            <BsCaretLeftSquareFill className="arrow arrow-left" />
+            <BsCaretLeftSquareFill className="arrow arrow-left" onClick={previousSlide} />
             {slides.map((item, idx) => {
                 return <img src={item.url} 
                     alt={item.alt} 
@@ -15,7 +23,7 @@ const SlideShow = ({slides}) => {
                     //Conditional className of slide, if the slide variable is the same as the index of this image, pass it a className of slide. Otherwise, pass it a className of slide and slide-hidden.//  
                     className={slide === idx ? "slide" : "slide slide-hidden"} />
         })}
-            <BsCaretRightSquareFill className="arrow arrow-right" />
+            <BsCaretRightSquareFill className="arrow arrow-right" onClick={nextSlide} />
             <span className="indicators" >
                 {slides.map((_, idx) => {
                     return <button key={idx} onClick={null} className={slide === idx ? "indicator" : "indicator indicator-inactive"} ></button>
