3c43c89fb1b0c0d732b4ef036c3a73531bd24ab0
Michelle Oh <ohmotrl@gmail.com>
2025-01-22 16:28:07 -0500

    Added isSubmitting property to formState so when user clicks submit button it will show loading

--- src/components/VolunteerForm.js
+++ src/components/VolunteerForm.js
@@ -7,13 +7,18 @@ const VolunteerForm = () => {
     //So we destructure the form variable and use the register function on the inputs.
     //We register with the inputs to work with the react hook form.
     //const form = useForm();
-    const { register, handleSubmit, formState: {errors} } = useForm();
+    const { register, handleSubmit, formState: {errors, isSubmitting} } = useForm();
     //handleSubmit from react hook forms will handle the default behavior of the form as well as handle the validating inputs for us.
     //To display errors to users, you can use the formState from the useForm hook. 
     //formState is an object you can destructure and get access to the errors
+    
+    //Another property you can access in the formState besides errors is isSubmitting.
+    //isSubmitting is a boolean that will be true when the form is submitting.
+    //You can make your submit button unaccessible while the form is submitting.
 
-    //Define onSubmit function
-    const onSubmit = (data) => {
+    //Convert onSubmit function into asynchronous function
+    const onSubmit = async (data) => {
+        await new Promise((resolve) => setTimeout(resolve, 1000));
         console.log(data);
     }
 
@@ -36,7 +41,7 @@ const VolunteerForm = () => {
             {errors.firstName && (<div className="text-red-500">{errors.firstName.message}</div>)}
             <input {...register("lastName", {required: "Last name is required", minLength: 2,})} type="lastName" placeholder="Last Name" />
             {errors.lastName && (<div className="text-red-500">{errors.lastName.message}</div>)}
-            <button type="submit">Submit Form</button>
+            <button disabled={isSubmitting} type="submit">{isSubmitting ? "Loading..." : "Submit Form"}</button>
         </form>
     );
 };
