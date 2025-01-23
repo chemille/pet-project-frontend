304911f4ef59eeeb3146d9a0dbe4fe1d7ff7696f
Michelle Oh <ohmotrl@gmail.com>
2025-01-22 22:35:23 -0500

    added form fields for dob and phone number with validation

--- src/components/VolunteerForm.js
+++ src/components/VolunteerForm.js
@@ -16,7 +16,7 @@ const VolunteerForm = () => {
         try {
             await new Promise((resolve) => setTimeout(resolve, 1000));  
             // throw new Error(); // This is causing an error when submitting the form.
-            console.log(data);
+            console.log(data); 
         } catch (error) {
             setError("root", {
                 message: "This email is already taken",
@@ -26,6 +26,15 @@ const VolunteerForm = () => {
 
     return (
         <form onSubmit={handleSubmit(onSubmit)}>
+            <label htmlFor="firstName">First Name: </label>
+            <input {...register("firstName", {required: "First name is required",})} type="text" placeholder="First Name" />
+            {errors.firstName && (<div className="text-red-500">{errors.firstName.message}</div>)}
+            
+            <label htmlFor="lastName">Last Name: </label>
+            <input {...register("lastName", {required: "Last name is required", minLength: 2,})} type="text" placeholder="Last Name" />
+            {errors.lastName && (<div className="text-red-500">{errors.lastName.message}</div>)}
+
+            <label htmlFor="email">Email: </label>
             <input
                 {...register("email", {
                     required: "Email is required", 
@@ -38,11 +47,64 @@ const VolunteerForm = () => {
                 })} 
                 type="text" placeholder="Email" />
             {errors.email && (<div className="text-red-500">{errors.email.message}</div>)}
-            <input {...register("firstName", {required: "First name is required",})} type="text" placeholder="First Name" />
-            {errors.firstName && (<div className="text-red-500">{errors.firstName.message}</div>)}
-            <input {...register("lastName", {required: "Last name is required", minLength: 2,})} type="text" placeholder="Last Name" />
-            {errors.lastName && (<div className="text-red-500">{errors.lastName.message}</div>)}
             
+            <label htmlFor="phoneNumber">Phone Number: </label> 
+            <input {...register("phoneNumber", {required: "Phone number is required", 
+                    validate: (value) => {
+                        // let isNum = /^\d+$/.test(value);
+                        if (value.length !== 10) {
+                            return "Phone number must be 10 digits";
+                        }
+                        if (!/^\d+$/.test(value)) {
+                            return "Phone number can only contain numbers"
+                        }
+                        return true;
+                    },
+                })} 
+                type="text" placeholder="9876543210" />
+            {errors.phoneNumber && (<div className="text-red-500">{errors.phoneNumber.message}</div>)}
+            
+            <label htmlFor="dob">Date of Birth: </label>
+            <input {...register("dob", {required: "Date of birth is required", valueAsDate: true,})} type="date" placeholder="Last Name" />
+            {errors.dob && (<div className="text-red-500">{errors.dob.message}</div>)}
+
+            <p>What type of volunteering would you like to do?</p>
+                <ul>
+                    <li>
+                    <label htmlFor="field-on-site">
+                    <input
+                        {...register("volunteerFor")}
+                        type="radio"
+                        value="on-site"
+                        id="field-on-site"
+                    />
+                    Volunteering on-site
+                    </label>
+                    </li>
+                <li>
+                <label htmlFor="field-foster">
+                    <input
+                        {...register("volunteerFor")}
+                        type="radio"
+                        value="foster"
+                        id="field-foster"
+                    />
+                    Fostering a pet
+                </label>
+                </li>
+                <li>
+                <label htmlFor="field-both">
+                    <input
+                        {...register("volunteerFor")}
+                        type="radio"
+                        value="both on-site and foster"
+                        id="field-both"
+                    />
+                    Both on-site and foster care
+                </label>                    
+                </li>
+                </ul>
+
             <button disabled={isSubmitting} type="submit">{isSubmitting ? "Loading..." : "Submit Form"}</button>
             {errors.root && (<div className="text-red-500">{errors.root.message}</div>)}
         </form>
